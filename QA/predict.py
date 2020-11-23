65f0a642751a607f90ae1bf2cb570764cb954921
ManavR123 <manav.rathod@berkeley.edu>
2020-11-23 03:27:40 -0500

    updated QA scripts

--- QA/predict.py
+++ QA/predict.py
@@ -5,41 +5,71 @@ import pandas as pd
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer
 from transformers.data.metrics.squad_metrics import compute_f1
 import torch
+import numpy as np
 from tqdm import tqdm
 
 
 def predict(filename, model_checkpoint):
-    df = pd.read_csv(filename)
-
+    BATCH_SIZE = 32
     tokenizer = AutoTokenizer.from_pretrained(
         "deepset/bert-large-uncased-whole-word-masking-squad2"
     )
     model = AutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
-    answers, scores = [], []
-    with tqdm(total=len(df)) as pbar:
-        for i, row in tqdm(df.iterrows()):
-            inputs = tokenizer(
-                row["context"],
-                row["question"],
-                add_special_tokens=True,
-                return_tensors="pt",
-            )
-            input_ids = inputs["input_ids"].tolist()[0]
-            text_tokens = tokenizer.convert_ids_to_tokens(input_ids)
-            answer_start_scores, answer_end_scores = model(**inputs)
-            answer_start = torch.argmax(answer_start_scores)
-            answer_end = torch.argmax(answer_end_scores) + 1
+
+    df = pd.read_csv(filename)
+    df = df.dropna()
+    contexts = df["context"].tolist()
+    questions = df["question"].tolist()
+    answers = df["answer"].tolist()
+
+    encodings = tokenizer(
+        contexts,
+        questions,
+        add_special_tokens=True,
+        return_tensors="pt",
+        truncation=True,
+        max_length=512,
+        padding=True,
+    )
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = model.to(device)
+    predicted_answers, scores = [], []
+    for i in tqdm(range(0, len(answers), BATCH_SIZE)):
+        batch_ids, batch_attn_masks, batch_ttids = (
+            encodings["input_ids"][i : i + BATCH_SIZE].to(device),
+            encodings["attention_mask"][i : i + BATCH_SIZE].to(device),
+            encodings["token_type_ids"][i : i + BATCH_SIZE].to(device),
+        )
+
+        answer_start_scores, answer_end_scores = model(
+            input_ids=batch_ids,
+            attention_mask=batch_attn_masks,
+            token_type_ids=batch_ttids,
+        )
+        answer_starts = torch.argmax(answer_start_scores, axis=1)
+        answer_ends = torch.argmax(answer_end_scores, axis=1) + 1
+        for j in range(BATCH_SIZE):
+            if i + j >= len(answers):
+                break
+            answer_start, answer_end = answer_starts[j].item(), answer_ends[j].item()
+            input_ids = encodings["input_ids"][i + j].tolist()
             answer = tokenizer.convert_tokens_to_string(
                 tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end])
             )
-            answers.append(answer)
-            if "answer" in row:
-                scores.append(compute_f1(row["answer"], answer))
-            pbar.update(1)
+            predicted_answers.append(answer)
+            scores.append(compute_f1(answers[i + j], answer))
 
-    df["predicted_answer"] = answers
+    df["predicted_answer"] = predicted_answers
+
     if scores:
         df["score"] = scores
+        print("=" * 50)
+        print(filename)
+        print("Cause Score: ", round(np.mean(scores[::2]), 2))
+        print("Effect Score: ", round(np.mean(scores[1::2]), 2))
+        print("Total Score: ", round(np.mean(scores), 2))
+        print("=" * 50)
     df.to_csv(filename)
 
 
